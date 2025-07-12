6572d928063a57af2682c0112b76c9d1c194b54f
n-sudo-mac <nsadnote22@gmail.com>
2025-07-12 22:16:45 +0900

    src

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 
 export default function Page() {
     return (
-        <div className="h-[100vh] flex flex-col gap-12 sm:gap-16 bg-red-900">
+        <div className="h-[100vh] flex flex-col gap-12 sm:gap-16 bg-purple-900">
            aaaaあいうえ
             
         </div>
