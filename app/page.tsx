1ebad354b049f8c335b96582950bfa6c0512f8d7
n-sudo-mac <nsadnote22@gmail.com>
2025-07-12 21:55:28 +0900

    TypeScript conversion completed

---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+
+export default function Page() {
+    return (
+        <div className="h-[100vh] flex flex-col gap-12 sm:gap-16 bg-blue-900">
+           aaaaあいうえ
+            
+        </div>
+    );
+}
