0448ca611dd3a3bd66067ca2e6d3a6eb42965181
n-sudo-mac <nsadnote22@gmail.com>
2025-07-15 02:47:20 +0900

    fix according to blog

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import './globals.css';
 import { Footer } from '@/components/footer';
 import Script from 'next/script';
+import TypekitLoader from './TypekitLoader';
 
 export const metadata = {
     title: {
@@ -21,22 +22,10 @@ export default function RootLayout({
                 <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="" />
                 <link rel="preconnect" href="https://use.typekit.net" />
                 <link href="https://fonts.googleapis.com/css2?family=Inter:wght@200;300;400;500;600;700;800&display=swap" rel="stylesheet" />
-                {/* Adobe Fonts JavaScript */}
-                <Script id="adobe-fonts" strategy="beforeInteractive">
-                  {`
-                    (function(d) {
-                      var config = {
-                        kitId: 'qpe7cyw',
-                        scriptTimeout: 3000,
-                        async: true
-                      },
-                      h=d.documentElement,t=setTimeout(function(){h.className=h.className.replace(/\\bwf-loading\\b/g,"")+" wf-inactive";},config.scriptTimeout),tk=d.createElement("script"),f=false,s=d.getElementsByTagName("script")[0],a;h.className+=" wf-loading";tk.src='https://use.typekit.net/'+config.kitId+'.js';tk.async=true;tk.onload=tk.onreadystatechange=function(){a=this.readyState;if(f||a&&a!="complete"&&a!="loaded")return;f=true;clearTimeout(t);try{Typekit.load(config)}catch(e){}};s.parentNode.insertBefore(tk,s)
-                    })(document);
-                  `}
-                </Script>
             </head>
             <body>
                 <div>
+                    <TypekitLoader />
                     <main>{children}</main>
                     <Footer />
                 </div>
