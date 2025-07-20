691371d4bbe4d0c66aaaea5d3caf4c25ae74f3ef
n-sudo-mac <nsadnote22@gmail.com>
2025-07-20 14:17:06 +0900

    optimize fonts

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -18,8 +18,6 @@ export default function RootLayout({
             <head>
                 <link rel="preconnect" href="https://fonts.googleapis.com" />
                 <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="" />
-                <link href="https://fonts.googleapis.com/css2?family=Noto+Sans+JP:wght@300;500;700;900&display=swap" rel="stylesheet" />
-                <link href="https://fonts.googleapis.com/css2?family=Noto+Sans:ital,wght@300;500;700;900&display=swap" rel="stylesheet" />
                 <link href="https://fonts.googleapis.com/css2?family=Zen+Kaku+Gothic+New:wght@300;400;500;700;900&display=swap" rel="stylesheet" />
                 <link href="https://fonts.googleapis.com/css2?family=Inter:wght@200;300;400;500;600;700;800&display=swap" rel="stylesheet" />
                 
