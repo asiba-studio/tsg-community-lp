6a878cf9d87fca38227ef3bf75ef6a572b256347
n-sudo-mac <nsadnote22@gmail.com>
2025-07-20 14:11:28 +0900

    reuse adobe fonts

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,5 @@
 import './globals.css';
+import Script from 'next/script'
 
 export const metadata = {
     title: {
@@ -7,11 +8,11 @@ export const metadata = {
     }
 };
 
-export default function RootLayout({ 
-    children 
-  }: {
+export default function RootLayout({
+    children
+}: {
     children: React.ReactNode
-  }) {
+}) {
     return (
         <html lang="ja">
             <head>
@@ -21,8 +22,24 @@ export default function RootLayout({
                 <link href="https://fonts.googleapis.com/css2?family=Noto+Sans:ital,wght@300;500;700;900&display=swap" rel="stylesheet" />
                 <link href="https://fonts.googleapis.com/css2?family=Zen+Kaku+Gothic+New:wght@300;400;500;700;900&display=swap" rel="stylesheet" />
                 <link href="https://fonts.googleapis.com/css2?family=Inter:wght@200;300;400;500;600;700;800&display=swap" rel="stylesheet" />
+                
+                <Script
+                    id="adobe-fonts"
+                    strategy="beforeInteractive"
+                >
+                    {`
+                        (function(d) {
+                            var config = {
+                                kitId: 'qpe7cyw', // ← 正しいKit IDに変更
+                                scriptTimeout: 3000,
+                                async: true
+                            },
+                            h=d.documentElement,t=setTimeout(function(){h.className=h.className.replace(/\\bwf-loading\\b/g,"")+" wf-inactive";},config.scriptTimeout),tk=d.createElement("script"),f=false,s=d.getElementsByTagName("script")[0],a;h.className+=" wf-loading";tk.src='https://use.typekit.net/'+config.kitId+'.js';tk.async=true;tk.onload=tk.onreadystatechange=function(){a=this.readyState;if(f||a&&a!="complete"&&a!="loaded")return;f=true;clearTimeout(t);try{Typekit.load(config)}catch(e){}};s.parentNode.insertBefore(tk,s)
+                        })(document);
+                    `}
+                </Script>
             </head>
-            <body>
+            <body className="font-sans">
                 <div>
                     <main>{children}</main>
                 </div>
