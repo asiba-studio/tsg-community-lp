c1095a071fc50a3efd791ebbabce9b71a4e35f05
n-sudo-mac <nsadnote22@gmail.com>
2025-07-21 23:47:27 +0900

    fix p5js hero

--- src/app/components/heroSection.tsx
+++ src/app/components/heroSection.tsx
@@ -3,9 +3,10 @@ export default function HeroSection() {
     return(
         <div className="w-full h-full -mt-14">
             <iframe 
-                src="https://openprocessing.org/sketch/2688456/embed/?plusEmbedHash=0f3726e3&userID=534668&plusEmbedTitle=true&show=sketch" 
+                src="/p5sketches/top-motion/index.html" 
                 width="100%" 
                 height="100%"
+                scrolling="no"
                 title="P5.js Sketch"
                 className="border-0"
             />
