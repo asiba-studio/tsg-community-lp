fc5ccc5387525f9c86476f18b0183bd37d730e42
n-sudo-mac <nsadnote22@gmail.com>
2025-07-23 16:17:08 +0900

    hide player sectino

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -103,9 +103,11 @@ export default async function Page() {
           </section>
 
           {/* Players Section */}
+          {/*}
           <section className="w-full section-spacing border-t border-border">
             <PlayerSection />
           </section>
+          */}
 
 
 
