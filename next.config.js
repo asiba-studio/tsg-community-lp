2489faf8df26eab8917e8f2c353691709f26fd42
n-sudo-mac <nsadnote22@gmail.com>
2025-07-29 20:43:04 +0900

    change not export

--- next.config.js
+++ next.config.js
@@ -1,9 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  output: 'export',
+  // output: 'export',
   images: {
-    unoptimized: true,
+    unoptimized: false,
   },
 };
 
