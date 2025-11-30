d215bfec50d60c0f2e05b15baf6f130de9f801dc
n-sudo-mac <nsadnote22@gmail.com>
2025-11-30 22:50:38 +0900

    fix opentalks

--- src/lib/openTalks.ts
+++ src/lib/openTalks.ts
@@ -1,64 +1,71 @@
+// src/lib/openTalks.ts
+
 import { OpenTalk } from "./types";
 
 export const OPEN_TALKS: OpenTalk[] = [
     {
+        id: "open-talk-01",
         slug: "open-talk-01",
-        link: "https://creativela.peatix.com/view",
+        type: "open-talks",
         title: "衝動する表現から始まる 領域の飛び越え方【9/28】",
         subtitle: "Creative-LAB. Open TALK #1",
         date: "2025-09-28",
         excerpt: "記憶のあり方に新たな循環をもたらす試みを行う仲村怜夏（yomiyomi）、Experimental Artで世界と対峙する自由の魔女エル（詳細）、軽トラに移動型の表現空間をつくる半田地寛（二畳建築）の3名。それぞれが、自身の衝動を起点にジャンルを越えて創造を実践してきた当事者です。",
         coverImage: "/images/open-talks/open-talk-01.png",
-        lang: "ja",
+        headerImage: "/images/open-talks/open-talk-01.png", // headerImageを追加
         tags: [],
-        content: ""
+        link: "https://creativela.peatix.com/view"
     },
     {
+        id: "open-talk-02",
         slug: "open-talk-02",
-        link: "https://creativelabopentalk2.peatix.com/view",
+        type: "open-talks",
         title: "Prototype Junkie【10/11】",
         subtitle: "Creative-LAB. Open TALK #2",
         date: "2025-10-11",
         excerpt: "登壇するのは、GOOD DESIGN NEW HOPE AWARD受賞歴のある加藤優、中川優奈、須藤望。自分のアイデアを異なる形で実践してきた3名と、プロトタイピングについて対話することで、参加者の「つくる」という営みをアップデートしていきます。",
         coverImage: "/images/open-talks/open-talk-02.png",
-        lang: "ja",
+        headerImage: "/images/open-talks/open-talk-02.png", // headerImageを追加
         tags: [],
-        content: ""
+        link: "https://creativelabopentalk2.peatix.com/view"
     },
     {
+        id: "open-talk-03",
         slug: "open-talk-03",
-        link: "https://creative-lab.peatix.com/view",
+        type: "open-talks",
         title: "日常と非日常の往復と、クリエイションの視座【10/23】",
         subtitle: "Creative-LAB. Open TALK #3",
         date: "2025-10-23",
         excerpt: "創造性とは、どこか遠くにあるものではなく、私たちが意識しない「日常の慣習」の中や、目を背けがちな「非日常の課題」の中に潜んでいます。この対極にある二つの世界を自由に行き来し、新しい価値を生み出す二人の若き才能が登壇します。",
         coverImage: "/images/open-talks/open-talk-03.png",
-        lang: "ja",
+        headerImage: "/images/open-talks/open-talk-03.png", // headerImageを追加
         tags: [],
-        content: ""
+        link: "https://creative-lab.peatix.com/view"
     },
     {
+        id: "open-talk-04",
         slug: "open-talk-04",
-        link: "https://creativelabtsg.peatix.com/view",
+        type: "open-talks",
         title: "「つくること」と「生きること」の重ね方とは？【11/2】",
         subtitle: "Creative-LAB. Open TALK #4",
         date: "2025-11-2",
         excerpt: "安全地帯を出て、まだ誰にも見せていない100%でつくってみる。 まだ知らない場所や人と出会い、その人の「生き方」に触れてみる。 好奇心でドキドキする瞬間に、共感をして身体が動いてしまう瞬間に、自分で気づいてみる。必要なのは、「正しい答え」を探すのではなく「自分の問い」から始めること。 自分の問いにまっすぐに答えるように「つくること」で、社会の課題や問題に出会うはずです。",
         coverImage: "/images/open-talks/open-talk-04.png",
-        lang: "ja",
+        headerImage: "/images/open-talks/open-talk-04.png", // headerImageを追加
         tags: [],
-        content: ""
+        link: "https://creativelabtsg.peatix.com/view"
     },
     {
+        id: "open-talk-05",
         slug: "open-talk-05",
-        link: "",
+        type: "open-talks",
         title: "詳細は随時公開【11/14 開催】",
         subtitle: "Creative-LAB. Open TALK #5",
         date: "2099-12-31",
         excerpt: "",
         coverImage: "/images/open-talks/open-talk-dummy.png",
-        lang: "ja",
+        headerImage: "/images/open-talks/open-talk-dummy.png", // headerImageを追加
         tags: [],
-        content: ""
+        link: ""
     }
 ];
